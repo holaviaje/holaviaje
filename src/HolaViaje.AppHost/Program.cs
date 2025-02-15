2dfcaf4b910562369172b283f74e76cd4bd11d95
Emilio <emilio@holaviaje.net>
2025-02-15 12:26:21 -0500

    Add PostgreSQL support and configuration
    Updated `HolaViaje.AppHost.csproj` to include `Aspire.Hosting.PostgreSQL` package version `9.0.0` for PostgreSQL functionality.
    
    Updated `appsettings.Development.json`:
    - Added `Parameters` section with `DbPassword`.

--- src/HolaViaje.AppHost/Program.cs
+++ src/HolaViaje.AppHost/Program.cs
@@ -2,6 +2,22 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<HolaViaje_Account>("account-api");
+#region PostgreSQL
+
+// Parameters for the builder
+var dbPassword = builder.AddParameter("DbPassword", true);
+
+// Add PostgreSQL to the container.
+var postgresql = builder.AddPostgres("postgresql-server", password: dbPassword);
+
+var accountDb = postgresql.AddDatabase("AccountDB");
+
+postgresql.WithDataVolume().WithPgAdmin();
+
+#endregion
+
+builder.AddProject<HolaViaje_Account>("account-api")
+    .WithReference(accountDb)
+    .WaitFor(accountDb);
 
 builder.Build().Run();
