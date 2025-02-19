86d4625877e20b62a635cb7279a9bd380df87949
Emilio <emilio@holaviaje.net>
2025-02-19 10:08:06 -0500

    Add Swagger UI

--- src/HolaViaje.Social/Program.cs
+++ src/HolaViaje.Social/Program.cs
@@ -1,3 +1,5 @@
+var devSpecificOrigins = "_devSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,12 +9,30 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Configure cross-origin requests
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: devSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:7089",
+                                              "http://localhost:5128").AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors(devSpecificOrigins);
+
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+
 }
 
 app.UseHttpsRedirection();
