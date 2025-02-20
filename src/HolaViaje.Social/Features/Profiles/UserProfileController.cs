0f8248f8456b6c31db8cc1cd9872db2217641ec2
Emilio <emilio@holaviaje.net>
2025-02-19 19:08:18 -0500

    Add ControllerCore base class and update UserProfileController
    Introduce ControllerCore with UserIdentity property for user identity retrieval.
    Update UserProfileController to inherit from ControllerCore.
    Add route and API controller attributes to UserProfileController.
    Implement endpoints for managing user profiles with authorization.
    Specify response types for various HTTP status codes.

---
+++ src/HolaViaje.Social/Features/Profiles/UserProfileController.cs
@@ -0,0 +1,102 @@
+﻿using HolaViaje.Social.Controllers;
+using HolaViaje.Social.Features.Profiles.Models;
+using HolaViaje.Social.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolaViaje.Social.Features.Profiles;
+
+[Route("[controller]")]
+[ApiController]
+public class UserProfileController(IUserProfileApplication profileApplication) : ControllerCore
+{
+    [Authorize]
+    [HttpGet]
+    [ProducesResponseType(typeof(UserProfileViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetAsync()
+    {
+        var result = await profileApplication.GetAsync(UserIdentity, UserIdentity);
+
+        return result.Match<IActionResult>(
+            profile => Ok(profile),
+            error => BadRequest(error));
+    }
+
+    [Authorize]
+    [HttpGet("{profileId}")]
+    [ProducesResponseType(typeof(UserProfileViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetAsync(long profileId)
+    {
+        var result = await profileApplication.GetAsync(profileId, UserIdentity);
+
+        return result.Match<IActionResult>(
+            profile => Ok(profile),
+            error => BadRequest(error));
+    }
+
+    [Authorize]
+    [HttpPut("{profileId}")]
+    [ProducesResponseType(typeof(UserProfileViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdateAsync(long profileId, UserProfileModel model)
+    {
+        var result = await profileApplication.UpdateAsync(profileId, model, UserIdentity);
+
+        return result.Match<IActionResult>(
+            profile => Ok(profile),
+            error => BadRequest(error));
+    }
+
+    [Authorize]
+    [HttpPut("{profileId}/Availability")]
+    [ProducesResponseType(typeof(UserProfileViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdateAvailabilityAsync(long profileId, AvailabilityModel model)
+    {
+        var result = await profileApplication.UpdateAvailabilityAsync(profileId, model, UserIdentity);
+
+        return result.Match<IActionResult>(
+            profile => Ok(profile),
+            error => BadRequest(error));
+    }
+
+    [Authorize]
+    [HttpPut("{profileId}/Place")]
+    [ProducesResponseType(typeof(UserProfileViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdatePlaceAsync(long profileId, PlaceInfoModel model)
+    {
+        var result = await profileApplication.UpdatePlaceAsync(profileId, model, UserIdentity);
+
+        return result.Match<IActionResult>(
+            profile => Ok(profile),
+            error => BadRequest(error));
+    }
+
+    [Authorize]
+    [HttpPut("{profileId}/SpokenLanguages")]
+    [ProducesResponseType(typeof(UserProfileViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdateSpokenLanguagesAsync(long profileId, List<SpokenLanguageModel> models)
+    {
+        var result = await profileApplication.UpdateSpokenLanguagesAsync(profileId, models, UserIdentity);
+
+        return result.Match<IActionResult>(
+            profile => Ok(profile),
+            error => BadRequest(error));
+    }
+}
