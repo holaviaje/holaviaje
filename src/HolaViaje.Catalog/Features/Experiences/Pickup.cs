eeabed1577eb2540eeeafd6fc59575935a22f9fb
Emilio <emilio@holaviaje.net>
2025-02-27 15:18:08 -0500

    Add Migration

--- src/HolaViaje.Catalog/Features/Experiences/Pickup.cs
+++ src/HolaViaje.Catalog/Features/Experiences/Pickup.cs
@@ -6,6 +6,6 @@ public record Pickup
 {
     public string? Address1 { get; set; }
     public string? Address2 { get; set; }
-    public PlaceInfo? Place { get; set; }
+    public PlaceInfo Place { get; set; } = new();
     public string? Details { get; set; }
 }
