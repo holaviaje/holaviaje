81046abcbfbced839696741867bcb9413e2fe335
Emilio <emilio@holaviaje.net>
2025-03-03 15:50:33 -0500

    Add Company Registration extensions

--- src/HolaViaje.Catalog/Features/Features.ServiceRegistration.cs
+++ src/HolaViaje.Catalog/Features/Features.ServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using HolaViaje.Catalog.Features.Experiences;
+﻿using HolaViaje.Catalog.Features.Companies;
+using HolaViaje.Catalog.Features.Experiences;
 
 namespace HolaViaje.Catalog.Features;
 
@@ -7,6 +8,7 @@ public static class ServiceRegistration
     public static IServiceCollection AddAllFeatures(this IServiceCollection services)
     {
         services.AddAutoMapper(typeof(ServiceRegistration).Assembly);
+        services.AddCompaniesFeature();
         services.AddExperiencesFeature();
         return services;
     }
